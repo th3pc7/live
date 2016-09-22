f4d48f8e5074e2b346b0711f77229fe3323dcae7
th3pc7 <zPadidaSz@gmail.com>
2016-09-23 01:57:15 +0700

    update bug helper

--- application/helpers/pp_helper.php
+++ application/helpers/pp_helper.php
@@ -11,8 +11,6 @@ function c_number($str_number,$point=2){
     else{ return null; }
 }
 
-<?php
-
 function get_video_data_from_youtube($my_id){
   $my_video_info = 'http://www.youtube.com/get_video_info?&video_id='.$my_id.'&asv=3&el=detailpage&hl=en_US';
   $ch_load = curl_init();
---
+++ application/logs/log-2016-09-23.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2016-09-23 01:56:39 --> UTF-8 Support Enabled
+DEBUG - 2016-09-23 01:56:39 --> No URI present. Default controller set.
+DEBUG - 2016-09-23 01:56:39 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2016-09-23 01:56:39 --> Severity: Parsing Error --> syntax error, unexpected '<' C:\wamp64\www\live\application\helpers\pp_helper.php 14
